9e2395c9e65e80c9aff9bf81576628791f62691c
uzair ullah <uzairullah397@gmail.com>
2025-05-24 01:44:36 +0500

    added facebook domain verifecation code and pixel view page code

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ import AuthProvider from './providers';
 import SiteSettingsProvider from '@/components/SiteSettingsProvider';
 import SiteStyleProvider from '@/components/SiteStyleProvider';
 import { getSiteSettings } from '@/lib/settings';
+import dynamic from 'next/dynamic';
+
+// Import PixelTracker using dynamic import with SSR disabled
+const PixelTracker = dynamic(() => import('@/components/PixelTracker'), { ssr: false });
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -40,13 +44,47 @@ export async function generateMetadata(): Promise<Metadata> {
     title: settings?.siteName || 'Andaz E Nu - Premium T-Shirts & Apparel',
     description: 'Shop the latest collection of premium t-shirts and apparel.',
     icons: settings?.faviconUrl ? { icon: settings.faviconUrl } : undefined,
+    // Add Facebook domain verification meta tag using the 'other' property
+    other: {
+      'facebook-domain-verification': 'mi5w78rv8dtjio82vx0bzx7vi2cwls',
+    },
+    // You can add other metadata like open graph, twitter, etc.
   };
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${montserrat.variable} ${roboto.variable} ${poppins.variable} ${robotoMono.variable}`}>
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1005322541746212');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1005322541746212&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
       <body className={`antialiased`}>
+        <PixelTracker />
         <ToastProvider />
         <AuthProvider>
           <SiteSettingsProvider>
